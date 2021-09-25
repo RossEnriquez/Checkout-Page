54e1e3edc740502248106f9e4168016493797196
Ross Enriquez <enriquez.ross091@gmail.com>
2021-09-24 23:19:59 -0400

    Fixed icons and textarea, added some button functionality (NEEDS FIX)

--- scripts.js
+++ scripts.js
@@ -1,9 +1,30 @@
-let counter = 0;
-document.getElementById("dec").addEventListener("click", function(){
-    counter--;
-    document.getElementById("counter-val-1").innerHTML = counter;
+let count0 = document.getElementById("counter-val-1");
+let count1 = document.getElementById("counter-val-2");
+
+document.getElementById("dec0").addEventListener("click", function(){
+    let counter = count0.value;
+    if (count0-1 >= 0){
+        count0.innerHTML = counter--;
+        count0.value = counter;
+    }
 });
-document.getElementById("inc").addEventListener("click", function(){
-    counter++;
-    document.getElementById("counter-val-1").innerHTML = counter;
+document.getElementById("inc0").addEventListener("click", function(){
+    let counter = count0.value;
+    count0.innerHTML = counter++;
+    count0.value = counter;
 });
+
+document.getElementById("dec1").addEventListener("click", function(){
+    let counter = count1.value;
+    if (count1-1 >= 0){
+        count1.innerHTML = counter--;
+        count1.value = counter;
+    }
+});
+document.getElementById("inc1").addEventListener("click", function(){
+    let counter = count1.value;
+    count0.innerHTML = counter++;
+    count0.value = counter;
+});
+
+
